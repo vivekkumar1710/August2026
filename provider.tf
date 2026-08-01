ad5f4c6f4c1cd72cbbbb798f0b6d255ae11e580e
vivekkumar1710 <vivekkumar1710@yahoo.in>
2026-08-02 01:45:13 +0530

    my fitst change

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "5.0.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {
+    
+  }
+}
